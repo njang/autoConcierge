b55e35e4a91b928ebd2d3a956bd9bfe1f4bd8348
njang <neo.w.jang@gmail.com>
2018-02-25 19:29:36 -0600

    Setup simple Django app to test NHTSA API

--- autoConcierge/urls.py
+++ autoConcierge/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.conf.urls import include, url
+from main_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'', include('main_app.urls')),
 ]
