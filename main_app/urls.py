2620a527fa24c26a70de5c8c1233e526064677fa
njang <neo.w.jang@gmail.com>
2018-03-01 21:28:04 -0600

    Enable remove car function

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     url(r'^signup/servicedriver/$', service_driver.SignUpView.as_view(), name='service_driver_signup'),
     url(r'^([0-9]+)/profile/$', main.profile, name='profile'),
     url(r'^car/add/$', main.add_car_form, name='add_car_form'),
-	url(r'^car/add/post_url/$', main.add_car, name='add_car'),
+    url(r'^car/add/post_url/$', main.add_car, name='add_car'),
     url(r'^car/([0-9]+)/$', main.show_car, name='show_car'),
+	url(r'^car/([0-9]+)/remove/$', main.remove_car, name='remove_car'),
 	url(r'^login/$', user.login_view, name='login'),		# display login page
 	url(r'^logout/$', user.logout_view, name='logout'),	# route to logout
 ]
